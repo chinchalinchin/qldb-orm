9dd3fda237754931302bd9e664a3e4c4437842c6
Grant Moore <gmoore@makpar.com>
2022-02-05 08:41:03 -0500

    various cleanup

--- main.py
+++ main.py
@@ -1,15 +1,9 @@
-import sys
-from qldb import settings
-from pyqldb.driver.qldb_driver import QldbDriver
-
-
-sys.path.append(settings.APP_DIR)
+from qldb.model import Model
 
 if __name__=="__main__":
-  from qldb.model import Model
   test = Model('test11')
   test.test_field = 'warning, this is a not a test'
   test.save()
   
   # from qldb.qldb import Driver
-  # QldbDriver(settings.LEDGER).execute_lambda(lambda executor: executor.execute_statement('CREATE TABLE ?', 'test3'))
+  # QldbDriver('innolab-Dev-test').execute_lambda(lambda executor: executor.execute_statement('CREATE TABLE test'))
