cfe2639f51664945cf3d8e14f424caad4336c59d
Grant Moore <gmoore@makpar.com>
2022-02-05 10:34:37 -0500

    testing update

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
+import sys
+import random
 from innoldb.qldb import Document
 
-if __name__=="__main__":
-  document = Document('innolab')
+specialities = ['Application Development', 'Cloud Migration', 'Data Analytics', 'Machine Learning']
+
+def create(document):
   document.company = 'Makpar'
   document.department = 'Innovation and Technology'
   document.location = 'Virginia'
   document.team = 'Innovation Lab'
-  document.specialty = 'Application Development'
+  document.specialty = specialities[random.randint(0, len(specialities) - 1)]
   document.members = {
     'UI/UX': 'Phung',
     'Solutions': 'Justin',
@@ -18,7 +21,32 @@ if __name__=="__main__":
     'Architect': 'Tariq'
   }
   document.save()
+
+def update_speciality(document):
+  document.specialty = specialities[random.randint(0, len(specialities) - 1)]
+  document.save()
+
+def update_prop(document, key, value):
+  setattr(document, key, value)
+  document.save()
+
+if __name__=="__main__":
+  document = Document('innolab')
+  routines = ['create']
+  if len(sys.argv) > 1:
+    routines += sys.argv[1:]
   
+  for routine in routines:
+    if routine == 'create':
+      create(document)
+    elif routine == 'update_spec':
+      update_speciality(document)
+    elif routine == 'update_prop_1':
+      update_prop(document, 'favorite_movie', 'children of men')
+    elif routine == 'update_prop_2':
+      update_prop(document, 'favorite_movie', 'pulp fiction')
+    elif routine == 'update_prop_3':
+      update_prop(document, 'favorite_movie', 'lords of salem')
 # if __name__==:""
   # from qldb.qldb import Driver
   # QldbDriver('innolab-Dev-test').execute_lambda(lambda executor: executor.execute_statement('CREATE TABLE test'))
