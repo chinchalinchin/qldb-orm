de77f3753deca0af61510c40298f4ca3bbcbdd30
Grant Moore <gmoore@makpar.com>
2022-02-07 10:41:56 -0500

    test for save

--- src/innoldb/tests/test_qldb.py
+++ src/innoldb/tests/test_qldb.py
@@ -15,7 +15,8 @@ from qldb import Document, Strut, Ledger
 ])
 def test_strut(kwargs,keys,values):
   keywords = vars(Strut(**kwargs))
-  assert all(keywords[act_key] == exp_val for act_key, exp_key, exp_val in zip(keywords.keys(), keys, values))
+  assert all(act_key == exp_key and keywords[act_key] == exp_val \
+              for act_key, exp_key, exp_val in zip(keywords.keys(), keys, values))
 
 @pytest.mark.parametrize('table,ledger',[
   ('howdy','ho'),
@@ -29,9 +30,32 @@ def test_ledger(table, ledger):
 @patch('qldb.Driver.tables')
 @patch('qldb.Driver.create_table')
 @patch('qldb.Driver.create_index')
-def test_document_driver(mock_create_index, mock_create_table, mock_tables, mock_driver):
-  Document(table='table', ledger='ledger')
+def test_document_driver_init(mock_create_index, mock_create_table, mock_tables, mock_driver):
+  document = Document(table='table', ledger='ledger')
   assert mock_driver.called
   assert mock_tables.called
   assert mock_create_table.called
   assert mock_create_index.called
+  assert document.table == 'table'
+  assert document.ledger == 'ledger'
+  assert document.id is not None
+
+@patch('qldb.Driver.driver')
+@patch('qldb.Driver.tables')
+@patch('qldb.Driver.create_table')
+@patch('qldb.Driver.create_index')
+@patch('qldb.Driver.query_by_fields', 
+        return_value=iter([{'lefty': 'loosey', 'righty': 'tighty'}]))
+@patch('qldb.Driver.insert', 
+        return_value=iter([{'lefty': 'loosey', 'righty': 'tighty'}]))
+def test_document_driver_save(mock_insert, mock_query, mock_create_index, mock_create_table, mock_tables, mock_driver):
+  document = Document(table='table', ledger='ledger')
+  document.lefty = 'loosey'
+  document.righty = 'tighty'
+  document.save()
+  assert mock_driver.called
+  assert mock_tables.called
+  assert mock_create_table.called
+  assert mock_create_index.called
+  assert mock_query.called
+  assert mock_insert.called
