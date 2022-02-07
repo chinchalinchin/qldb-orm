6075fb8a1954a2664adae6f192b83c0e9f3c074a
Grant Moore <gmoore@makpar.com>
2022-02-06 22:12:20 -0500

    tests for ledger

--- src/innoldb/tests/test_qldb.py
+++ src/innoldb/tests/test_qldb.py
@@ -6,7 +6,7 @@ APP_DIR = os.path.dirname(TEST_DIR)
 sys.path.append(APP_DIR)
 
 import pytest
-from qldb import Strut, Ledger
+from qldb import Strut, Ledger, Document, Driver
 
 @pytest.mark.parametrize('kwargs,keys,values',[
   ({'a':'b'},['a'], ['b']),
@@ -23,3 +23,14 @@ def test_strut(kwargs,keys,values):
 ])
 def test_ledger(table, ledger):
   assert Ledger(table, ledger).table == table and Ledger(table, ledger).ledger == ledger
+
+# def test_document_driver(mocker):
+#   driver_stub = mocker.stub(name='Driver.drive')
+#   tables_stub = mocker.stub(name='Driver.tables')
+#   table_stub = mocker.stub(name='Driver.create_table')
+#   index_stub = mocker.stub(name='Driver.create_index')
+#   Document(table='table', ledger='ledger')
+#   assert driver_stub.call_count == 3
+#   assert tables_stub.call_count == 1
+#   assert table_stub.call_count == 1
+#   assert index_stub.call_count == 1
