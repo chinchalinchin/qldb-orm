b94cc8d3493dd38b47078e53837a2fe6c8ecdf52
Grant Moore <gmoore@makpar.com>
2022-02-08 16:05:33 -0500

    new lines in doc strings

--- src/innoldb/qldb.py
+++ src/innoldb/qldb.py
@@ -213,7 +213,9 @@ class Document(QLDB):
 
 
 class Query(QLDB):
-    """Object that represents a **PartiQL** query. Get initialized on a particular `table` and `ledger`. Methods will return results formatted as collections of `innoldb.qldb.Document`.
+    """Object that represents a **PartiQL** query. Get initialized on a particular `table` and `ledger`. 
+    
+    Methods will return results formatted as collections of `innoldb.qldb.Document`.
     """
 
     def __init__(self, table, ledger=settings.LEDGER):
@@ -278,7 +280,9 @@ class Query(QLDB):
         return self._to_documents(Driver.query_by_fields(Driver.driver(self.ledger), self.table, **kwargs))
 
     def find_in(self, **kwargs):
-        """Filter `innoldb.qldb.Document` objects by the provided fields. This method accepts `**kwargs` arguments for the field name and values, but the values must be an array. The document fields must belong to the array associated with the field in the `**kwargs`. See below for example
+        """Filter `innoldb.qldb.Document` objects by the provided fields. This method accepts `**kwargs` arguments for the field name and values, but the values must be an array. 
+        
+        The document fields must belong to the array associated with the field in the `**kwargs`. See below for example.
 
         :param kwargs: Fields by which to filter the query.
         :return: List of `innoldb.qldb.Document`
